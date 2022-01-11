aa1c47d382f46c29a59d9618a270ae72953c23e1
M00784017 <ts937@live.mdx.ac.uk>
2022-01-11 15:17:14 +0000

    Update UIExample(11-01-21).py

--- Fadecandy_2400-main/Fadecandy_2400-main/examples/UIExample(11-01-21).py
+++ Fadecandy_2400-main/Fadecandy_2400-main/examples/UIExample(11-01-21).py
@@ -1,8 +1,30 @@
-value = input("welcome to the menue. options are listed below:")
+value = input("welcome to the menue. options are listed below: /n/t 1.Roll /n/t 2.Scroll /n Type your choice")
 print("The value you input is:", value)
 print(f'it is of type{type(value)}.')
-value=int(value)
-print("The converted is :", value)
-print(f'it is of type {type(value)}.')
+def func1(val):
+    return val**val
+def func2(val):
+    return val**val
+def func3(val):
+    return val**val
+while True:
+   if value.isdigit()==True:
+       
+        value=int(value)
+        break
+    
+else:
+        
+    print("invalid input, please provide an integer:")
+
+#print("The converted is :", value)
+#print(f'it is of type {type(value)}.')
+    
+if value==1:
+    func1(val)
+elif value==2:
+    func2(value)
+elif value==3:
+    func3(value)
 
 
