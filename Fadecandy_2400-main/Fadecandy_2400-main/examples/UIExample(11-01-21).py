602b2372e4e0736612d02c82340e2d638f221e07
M00784017 <ts937@live.mdx.ac.uk>
2022-01-11 14:52:04 +0000

    update with string format

--- Fadecandy_2400-main/Fadecandy_2400-main/examples/UIExample(11-01-21).py
+++ Fadecandy_2400-main/Fadecandy_2400-main/examples/UIExample(11-01-21).py
@@ -1,3 +1,8 @@
-value=input()
+value = input("welcome to the menue. options are listed below:")
+print("The value you input is:", value)
+print(f'it is of type{type(value)}.')
+value=int(value)
+print("The converted is :", value)
+print(f'it is of type {type(value)}.')
 
-print(value)
+
