ed553a17fd6d34cde9dafc56796cc37e98961d24
M00784017 <ts937@live.mdx.ac.uk>
2022-01-11 15:34:08 +0000

    Update UIExample(11-01-21).py

--- Fadecandy_2400-main/Fadecandy_2400-main/examples/UIExample(11-01-21).py
+++ Fadecandy_2400-main/Fadecandy_2400-main/examples/UIExample(11-01-21).py
@@ -1,30 +1,34 @@
-value = input("welcome to the menue. options are listed below: /n/t 1.Roll /n/t 2.Scroll /n Type your choice")
-print("The value you input is:", value)
-print(f'it is of type{type(value)}.')
+value = input('''Welcome to the menu. Options are listed below:,
+              \t 1.Roll
+              \t 2. Sweep
+              \t 3. Scroll
+              Type the number of your choice and press Enter.''') #\n - newline; \t - tab
+
+#print("The value you input is:", value)
+#print(f'it is of type {type(value)}.')
+
 def func1(val):
     return val**val
 def func2(val):
     return val**val
 def func3(val):
     return val**val
-while True:
-   if value.isdigit()==True:
-       
-        value=int(value)
-        break
-    
-else:
-        
-    print("invalid input, please provide an integer:")
 
-#print("The converted is :", value)
+while True:
+    if value.isdigit() == True: # .isdigit() 
+        value = int(value)
+        break # on correct value datatype: exit the loop
+    else:
+        value=input("invalid input, please provide an integer:") #ask for a new value
+#print("The converted is:", value)
 #print(f'it is of type {type(value)}.')
-    
-if value==1:
-    func1(val)
-elif value==2:
-    func2(value)
-elif value==3:
-    func3(value)
+
+#compare numeric value to choices available, perform assicoated function or sequence.
+if value == 1:
+    print(func1(value))
+elif value == 2:
+    print(func2(value))
+elif value == 3:
+    print(func3(value))
 
 
