4259bf21f3916e2cc90ee67e25c1ea69fcc10f68
M00784017 <ts937@live.mdx.ac.uk>
2022-01-16 15:16:28 +0000

    Added Ireland Flag to the list of animations alongside Palestine and England.

--- Fadecandy_2400-main/Fadecandy_2400-main/Project work animations/Project Trial 2.py
+++ Fadecandy_2400-main/Fadecandy_2400-main/Project work animations/Project Trial 2.py
@@ -15,11 +15,6 @@ E=range(304,360)
 G=range(0,27)
 D=range(33,87)
 D1=range(93,120)
-grey=(100,100,100)  #dark grey color number 
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
-green = (0,255,0)
 D2=range(240,267)
 D3=range(273,327)
 D4=range(333,360)
@@ -27,8 +22,13 @@ D5=range(27,33)
 D6=range(87,93)
 D7=range(267,273)
 D8=range(327,333)
-
- 
+#defining colour numbers in rgb
+grey=(100,100,100)  #dark grey color number 
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+orange=(255,165,0)
  
 
 
@@ -75,20 +75,34 @@ def England():
             led_colour[number] = red   
         if (number % 360 >= 120) and (number % 360 < 240):
             led_colour[number] = red
+def Ireland():
+    for number, colour in enumerate(led_colour):#iterate through 360 LEDs (with an index (count))
+        #green part of flag:
+        if number % 60 <= 20:
+            led_colour[number] = green
+        #white part of flag:
+        if (number % 60 <= 40) and (number % 60 >= 20):
+            led_colour[number] = white
+        #red part of flag:
+        if (number % 60 >= 40) and (number % 60 <= 80):
+            led_colour[number] = orange
+
 def menu(): 
     
     print('\nEnter the number of the flag you wish to be displayed:')
-    print('\n1. Palestine\n2.England')
+    print('\n1. Palestine\n2. England\n3. Ireland')
     x = int(input()) #the value from the user is an integer
     while(x not in (1,2,3,4)):
         
-        print('\nPlease enter a valid input!\n')
+        print('\nPlease enter a value between 1-4!\n')
         x = int(input()) 
     
     if x == 1:
         return Palestine() #at the moment this is the only country flag I have
     elif x==2:
         return England()
+    elif x==3:
+        return Ireland()
     
 while True:
     menu()
