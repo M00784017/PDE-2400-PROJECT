b37b68e6b91cfe42ab31c0ed7886bf2b827ba3cf
M00784017 <ts937@live.mdx.ac.uk>
2022-01-18 22:30:26 +0000

    some revisions of previous excercises

--- Fadecandy_2400-main/Fadecandy_2400-main/examples/in class excercises/basics(11-01-21).py
+++ Fadecandy_2400-main/Fadecandy_2400-main/examples/in class excercises/basics(11-01-21).py
@@ -7,26 +7,26 @@ client.put_pixels(leds)
 client.put_pixels(leds)
 
 
-for led in range(60):
-    leds[led]=(255,0,0)
-    time.sleep(0.1)
+for led in range(360):
+    leds[led]=(255,176,0)
+    time.sleep(0.01)
     client.put_pixels(leds)
     
 led=0
-while led<60:
-    leds[59-led]=(0,255,0)
+while led<360:
+    leds[359-led]=(0,255,0)
     time.sleep(0.1)
     client.put_pixels(leds)
     led=led+1
 
 led=0
 #making it more compact
-while led<60:
+while led<360:
     for rows in range(6):
         leds[led+ rows*60]=(0,0,255)
     #leds[led]=(0,0,255)
     #leds[led+60]=(0,0,255)
-    time.sleep(0.1)
+    time.sleep(0.01)
     client.put_pixels(leds)
     led=led+1
 
