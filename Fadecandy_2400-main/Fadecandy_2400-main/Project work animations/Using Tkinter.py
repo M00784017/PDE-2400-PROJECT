dcf0eeaef8c97c93275f611c05b02f1a6995a5e1
M00784017 <ts937@live.mdx.ac.uk>
2022-01-29 23:15:12 +0000

    Update Using Tkinter.py

--- Fadecandy_2400-main/Fadecandy_2400-main/Project work animations/Using Tkinter.py
+++ Fadecandy_2400-main/Fadecandy_2400-main/Project work animations/Using Tkinter.py
@@ -1,14 +1,14 @@
-import tkinter as tk
+import tkinter as tk #tk for easier use
 
 root = tk.Tk()
-root.title('PDE 2400')
+root.title('PDE 2400') #title for the block
 
 Input = tk.Label(root, text = "Welcome, press on the animation you would like to see" , foreground = 'white',
                 background = 'blue', width = 50, height = 2).grid(row = 1, column = 0)
 
 
 def Project_Trial_2():
-    import Project_Trial_2
+    import Project_Trial_2 #import this file when button is pressed
     
 
 PT2 = tk.Button(root, text = 'PT2', command = Project_Trial_2).grid(row = 4, column = 0)
