887858a6c277ed1fdb0aecb436df71dd76de70bc
M00784017 <ts937@live.mdx.ac.uk>
2022-02-01 10:10:19 +0000

    Update Using Tkinter.py

--- Fadecandy_2400-main/Fadecandy_2400-main/Project work animations/Using Tkinter.py
+++ Fadecandy_2400-main/Fadecandy_2400-main/Project work animations/Using Tkinter.py
@@ -4,7 +4,7 @@ root = tk.Tk()
 root.title('PDE 2400') #title for the block
 
 Input = tk.Label(root, text = "Welcome, press on the animation you would like to see" , foreground = 'white',
-                background = 'blue', width = 50, height = 2).grid(row = 1, column = 0)
+                background = 'blue', width = 50, height = 2).grid(row = 1, column = 0)#asks the user to press on the animation, the width and height were to match the length of the text
 
 
 def Project_Trial_2():
